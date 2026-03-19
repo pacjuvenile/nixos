6360791030cf695d75bb77828d83165ec73961d4
packjuvenile <3283890195@qq.com>
2026-03-19 23:53:17 +0800

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,58 +1,58 @@
 {
-  description = "NixOS-WSL configuration";
+	description = "NixOS-WSL configuration";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+		nixos-wsl.url = "github:nix-community/nixos-wsl";
+		neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+	};
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-wsl, neovim-nightly-overlay, ... }: {
-    nixosConfigurations = {
-      "nixos-wsl" = let
-        system = "x86_64-linux"; 
-      in nixpkgs.lib.nixosSystem {
-        inherit system; 
-        specialArgs = {
-          inherit inputs system; 
-          unstable = nixpkgs-unstable.legacyPackages.${system};
-        };
-        modules = [
-          nixos-wsl.nixosModules.wsl
-          ({ config, lib, pkgs, inputs, unstable, ... }: {
-            imports = [];
+	outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixos-wsl, neovim-nightly-overlay, ... }: {
+		nixosConfigurations = {
+			"nixos-wsl" = let
+				system = "x86_64-linux"; 
+			in nixpkgs.lib.nixosSystem {
+				inherit system; 
+				specialArgs = {
+					inherit inputs system; 
+					unstable = nixpkgs-unstable.legacyPackages.${system};
+				};
+				modules = [
+					nixos-wsl.nixosModules.wsl
+					({ config, lib, pkgs, inputs, unstable, ... }: {
+						imports = [];
 
-            wsl.enable = true;
-            wsl.defaultUser = "pacjuvenile";
+						wsl.enable = true;
+						wsl.defaultUser = "pacjuvenile";
 						system.stateVersion = "25.05";
 
-            time.timeZone = "Asia/Shanghai";
+						time.timeZone = "Asia/Shanghai";
 
-            systemd.services.nix-daemon.environment = {
-              http_proxy = "http://172.21.160.1:7890";
-              https_proxy = "http://172.21.160.1:7890";
-            };
+						systemd.services.nix-daemon.environment = {
+							http_proxy = "http://172.21.160.1:7890";
+							https_proxy = "http://172.21.160.1:7890";
+						};
 
-            environment.systemPackages = with pkgs; [
-              unzip gzip gnutar xclip git curl wget fd tree ripgrep opencv ffmpeg-full
-              zsh fzf zellij yazi starship
-              nodejs_24 deno yarn bun tree-sitter typescript-language-server
-              gcc gnumake cmake rustup rbenv python313
-              lua-language-server marksman pyright svls yaml-language-server ruby-lsp
+						environment.systemPackages = with pkgs; [
+							unzip gzip gnutar xclip git curl wget fd tree ripgrep opencv ffmpeg-full
+							zsh fzf zellij yazi starship
+							nodejs_24 deno yarn bun tree-sitter typescript-language-server
+							gcc gnumake cmake rustup rbenv python313
+							lua-language-server marksman pyright svls yaml-language-server ruby-lsp
 							kmod util-linux ddrescue testdisk
-            ];
+						];
 
-            programs.neovim = {
-              enable = true;
-              package = inputs.neovim-nightly-overlay.packages.${system}.default;
-            };
+						programs.neovim = {
+							enable = true;
+							package = inputs.neovim-nightly-overlay.packages.${system}.default;
+						};
 
-            programs.zsh = { 
+						programs.zsh = { 
 							enable = true; 
 						};
 
-            programs.direnv = { 
+						programs.direnv = { 
 							enable = true; 
 						};
 
@@ -62,22 +62,22 @@
 							defaultNetwork.settings.dns_enabled = true;
 						};
 
-            nix = {
-              package = pkgs.nix;
-              extraOptions = ''
-                experimental-features = nix-command flakes
-              '';
-            };
+						nix = {
+							package = pkgs.nix;
+							extraOptions = ''
+								experimental-features = nix-command flakes
+							'';
+						};
 
-            users.users.pacjuvenile = {
-              isNormalUser = true;
-              home = "/home/pacjuvenile";
-              extraGroups = [ "wheel" "networkmanager" ];
-              shell = pkgs.zsh;
-            };
-          })
-        ];
-      };
-    };
-  };
+						users.users.pacjuvenile = {
+							isNormalUser = true;
+							home = "/home/pacjuvenile";
+							extraGroups = [ "wheel" "networkmanager" ];
+							shell = pkgs.zsh;
+						};
+					})
+				];
+			};
+		};
+	};
 }
