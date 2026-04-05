31ca891442371bb7200dc509da383045e3047ce2
Test <test@test.com>
2026-04-05 17:10:26 +0800

    modified:   hosts/wsl/default.nix

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -33,7 +33,7 @@
     opencv ffmpeg-full
 
     # Shell & 终端
-    zsh fzf zellij yazi starship tuios
+    zsh fzf tmux yazi starship
 
     # Node.js 生态
     nodejs_24 deno yarn bun tree-sitter typescript-language-server
